35e667fd2e393c4d35804742173966a99d8a41f8
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-05-19 09:37:29 +0530

    Initial Commit

---
+++ loginScreenWithSteticPARAMITAR__API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginScreenWithSteticPARAMITAR__API
+//
+//  Created by CodeInfoWay CodeInfoWay on 5/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
